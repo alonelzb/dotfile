7120085d8f753c074b4d053909b8ddec50bef011
alonelzb <zaiboluo@gmail.com>
2023-05-20 17:15:45 +0800

    draw out alias

---
+++ dot_config/zsh/aliases.zsh
@@ -0,0 +1,37 @@
+# alass
+alias ..='cd ..'
+alias h='history'
+# lsd
+alias l='lsd -la'
+alias ls='lsd'
+alias ll='lsd -l'
+alias lt='lsd -lt'
+alias lS='lsd -lS'
+alias la='lsd -a'
+alias ltree='lsd --tree'
+alias pip='pip3'
+alias py='python3'
+# nvim
+alias nv='nvim'
+alias -s py=nv
+alias -s html=nv
+alias -s js=nv
+alias -s ts=nv
+alias -s vue=nv
+alias zshrc='nvim ${ZDOTDIR}/.zshrc'
+alias sr='source ${ZDOTDIR}/.zshrc'
+alias clash='~/.config/clash/clash-linux &'
+alias httpd='http -d'
+alias httpb='http -b'
+# git
+alias g='git'
+
+alias ga='git add'
+alias gaa='git add --all'
+alias gb='git branch'
+alias gba='git branch --all'
+alias gcb='git checkout -b'
+alias gl='git pull'
+alias gm='git merge'
+alias gp='git push'
+# }}}
